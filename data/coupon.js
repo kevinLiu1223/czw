bde237e710d836f2b2b5c567b87802a0fa52cd1c
ziguang <ziguang@scapital.com>
2017-11-28 18:10:11 +0800

    优惠券详情页

---
+++ data/coupon.js
@@ -0,0 +1,42 @@
+var cupon_database = [
+    {
+        src: "../pages/images/coupon_cover_small1.jpeg"
+    },
+    {
+        src: "../pages/images/coupon_cover_small2.jpeg"
+    },
+    {
+        src: "../pages/images/coupon_cover_small3.jpeg"
+    },
+    {
+        src: "../pages/images/coupon_cover_small4.jpeg"
+    },
+    {
+        src: "../pages/images/coupon_cover_small5.jpeg"
+    },
+    {
+        src: "../pages/images/coupon_cover_small6.jpeg"
+    },
+    {
+        src: "../pages/images/coupon_cover_small7.jpeg"
+    },
+    {
+        src: "../pages/images/coupon_cover_small8.jpeg"
+    },
+    {
+        src: "../pages/images/coupon_cover_small9.jpeg"
+    },
+    {
+        src: "../pages/images/coupon_cover_small10.jpeg"
+    },
+    {
+        src: "../pages/images/coupon_cover_small11.jpeg"
+    },
+    {
+        src: "../pages/images/coupon_cover_small12.jpeg"
+    }
+]
+
+module.exports = {
+    postList: cupon_database
+}
---
+++ pages/coupon/coupon.js
@@ -0,0 +1,14 @@
+Page({
+    data:{
+
+    },
+    onLoad:function () {
+
+    },
+    onTap:function (event) {
+        wx.navigateTo({
+            url: '../coupon_detail/coupon_detail?id=1'
+        })
+    }
+})
+

4a940f3c80c41c59dc57966f596885977efc31f9
ziguang <ziguang@scapital.com>
2017-12-02 16:43:58 +0800

    卡券间的跳转

--- pages/coupon_center/coupon_center.js
+++ pages/coupon_center/coupon_center.js
@@ -2,8 +2,8 @@ var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
 
 Page({
     data: {
-        tabs: ["未使用", "已使用", "已过期"],
-        activeIndex: 1,
+        tabs: ["购买票券", "优惠卡券", "不可用卡券"],
+        activeIndex: 0,
         sliderOffset: 0,
         sliderLeft: 0
     },
--- pages/coupon_detail/coupon_detail.js
+++ pages/coupon_detail/coupon_detail.js
@@ -10,26 +10,41 @@ Page({
     openConfirm: function () {
         this.setData({
             color:'',
-            text:'立即使用',
+            text:'领取成功',
             backgroundColor:'#eeeeee'
         })
-        wx.showModal({
-            title: '车蛛网',
-            content: '请关注车蛛网公众号',
-            confirmText: "关注使用",
-            cancelText: "取消使用",
-            success: function (res) {
-                console.log(res);
-                if (res.confirm) {
-                    wx.showToast({
-                        title: '关注成功',
-                        icon: 'success',
-                        duration: 3100
-                    });
-                }else{
-                    console.log('用户点击辅助操作')
-                }
+        wx.showToast({
+            title: '领取成功',
+            icon: 'loading',
+            duration: 3000,
+            'success': function () {
+                setTimeout(function () {
+                    wx.navigateTo({
+                        url: '../coupon_center/coupon_center'
+                    })
+                    // wx.navigateTo({
+                    //     url:'../coupon_center/coupon_center'
+                    // })
+                },2000)
             }
         });
+        // wx.showModal({
+        //     title: '车蛛网',
+        //     content: '请关注车蛛网公众号',
+        //     confirmText: "关注使用",
+        //     cancelText: "取消使用",
+        //     success: function (res) {
+        //         console.log(res);
+        //         if (res.confirm) {
+        //             wx.showToast({
+        //                 title: '关注成功',
+        //                 icon: 'success',
+        //                 duration: 3100
+        //             });
+        //         }else{
+        //             console.log('用户点击辅助操作')
+        //         }
+        //     }
+        // });
     }
 })
